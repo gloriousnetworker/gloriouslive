828dbd7738794008f418e097f46026299cf5e9d3
gloriousnetworker <iniubongudofot@gmail.com>
2024-10-05 02:20:09 +0100

    Initial commit: Set up Node.js backend for Contact form with Firebase

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -41,7 +41,7 @@ const Contact: React.FC = () => {
 
     // Send data to the backend
     try {
-        const response = await fetch('http://localhost:5000/api/contact', {
+        const response = await fetch('https://gloriouslive.vercel.app/api/contact', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
